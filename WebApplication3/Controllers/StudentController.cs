7b78795e416164065b3957fd2721363633f16981
sriharibanoth <sreeharibanoth1203@gmail.com>
2021-12-31 09:55:41 +0530

    Add project files.

---
+++ WebApplication3/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetStudentId()
+        {
+            GetStudentName();
+            return View();
+        }
+        public IActionResult GetStudentName()
+        {
+            return View();
+        }
+    }
+}
